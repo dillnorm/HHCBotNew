0b53cade6295215a8cdefcdf09efbd52093f3ebe
dillnorm <31057595+dillnorm@users.noreply.github.com>
2019-10-16 11:52:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ function respond() {
         console.log("Testing PT Plan");
         this.res.writeHead(200);
         console.log("Posting PT Plan");
-        postMessage("@"+request.name+" "+ptDay());
+        postMessage("@"+request.name+" \n"+ptDay());
         this.res.end();  
      }
      else if(Regexp3.test(request.text)){
